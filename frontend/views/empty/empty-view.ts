2d1ee6a52ea747f159c624581d52e2692f8afc02
Andreas Maschke <thargor6@googlemail.com>
2022-01-15 16:39:06 +0100

    added possiblitty to re-render and to select an example-flame to (re)-render

--- frontend/views/empty/empty-view.ts
+++ frontend/views/empty/empty-view.ts
@@ -1,25 +1,40 @@
 import {html, PropertyValues} from 'lit';
-import { customElement } from 'lit/decorators.js';
+import {customElement, property, query} from 'lit/decorators.js';
 import { View } from '../../views/view';
 
 import '@polymer/paper-slider/paper-slider'
 import '@vaadin/vaadin-button'
+import '@vaadin/vaadin-text-field'
 
 import {FlameRenderer} from '../../flames/renderer/flame-renderer'
 import {FlamesEndpoint} from "Frontend/generated/endpoints";
 import {FlameMapper} from '../../flames/model/mapper/flame-mapper'
+import {TextFieldValueChangedEvent} from "@vaadin/text-field/src/vaadin-text-field";
+import {HasValue} from "@vaadin/form";
 
 @customElement('empty-view')
 export class EmptyView extends View {
+  @query("#canvas-container")
+  canvasContainer!: HTMLDivElement;
+
+  @property()
+  flameName = 'example02'
+
   render() {
+
     return html`
 
-      <vaadin-button @click="${this.onClick}">Test</vaadin-button>
 
-<canvas id="screen1" width="512" height="512"></canvas>
 
+      <div id="canvas-container">
+
+        
+        <canvasx id="screen1" width="512" height="512"></canvasx>
+      </div>
 <div id="overlay">
    <form name="controls">
+     <vaadin-text-field value="${this.flameName}" @change="${(event: Event)=>this.flameName=(event.target as HasValue<string>).value!}"></vaadin-text-field>
+     <vaadin-button @click="${this.onClick}">Render</vaadin-button>
     <paper-slider  id="brightness" step="0.0001" value="2.2" min="0" max="4"></paper-slider>
     <paper-slider  id="param1" step="0.1" value="2.5" min="0" max="10.0"></paper-slider>
     <label><input type="radio" name="displayMode" value="flame" checked="checked"> Flame</label>
@@ -48,29 +63,39 @@ export class EmptyView extends View {
 
   initFlag = false;
 
+  renderFlame(flameName: string) {
+    this.canvasContainer.innerHTML = '';
+
+    var brightnessElement = document.querySelector("#brightness") as HTMLElement;
+    var param1Element = document.querySelector("#param1") as HTMLElement;
+    var radioButtonElements = document.getElementsByName('displayMode') ;
+    // var canvas = document.getElementById("screen1")  as HTMLCanvasElement;
+
+    var canvas = document.createElement('canvas');
+
+    canvas.id = "screen1";
+    canvas.width = 512;
+    canvas.height = 512;
+    this.canvasContainer.appendChild(canvas);
+
+    FlamesEndpoint.getExampleFlame(flameName).then( flame=> {
+      const renderer = new FlameRenderer(canvas, FlameMapper.mapFromBackend(flame), brightnessElement, radioButtonElements, param1Element);
+      renderer.drawScene()
+    })
+  }
+
   protected firstUpdated(_changedProperties: PropertyValues) {
     super.firstUpdated(_changedProperties);
 
-    if(!this.initFlag) {
+    if(!this.initFlag && this.canvasContainer) {
       this.initFlag = true;
-
-      var brightnessElement = document.querySelector("#brightness") as HTMLElement;
-      var param1Element = document.querySelector("#param1") as HTMLElement;
-      var radioButtonElements = document.getElementsByName('displayMode') ;
-      var canvas = document.getElementById("screen1")  as HTMLCanvasElement;
-
-
-
-      FlamesEndpoint.getExampleFlame("example04").then( flame=> {
-        const renderer = new FlameRenderer(canvas, FlameMapper.mapFromBackend(flame), brightnessElement, radioButtonElements, param1Element);
-        renderer.drawScene()
-      })
-
+      this.renderFlame(this.flameName)
     }
 
   }
 
   onClick = ()=> {
-    FlamesEndpoint.getExampleFlame("example04").then( f=> console.log("FLAME:",  FlameMapper.mapFromBackend(f)))
+    this.renderFlame(this.flameName)
+    //FlamesEndpoint.getExampleFlame("example04").then( f=> console.log("FLAME:",  FlameMapper.mapFromBackend(f)))
   }
 }
