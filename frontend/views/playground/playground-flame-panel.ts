f80af204d0cd6b5f49596e015b0d09d49ccc1a6f
Andreas Maschke <thargor6@googlemail.com>
2022-02-02 01:00:28 +0100

    some new examples

--- frontend/views/playground/playground-flame-panel.ts
+++ frontend/views/playground/playground-flame-panel.ts
@@ -79,7 +79,9 @@ FECCB3FECCB3FECCB3FECCB3FECCB3FECCB3840802840802840802840802840802840802
   @state()
   flameName = 'example08'
 
-  flameNames = ['example01', 'example02', 'example03', 'example04', 'example05', 'example06', 'example07', 'example08', 'example10', 'example11', 'example12', 'example14', 'example16', 'example17', 'example20', 'example21']
+  flameNames = ['example01', 'example02', 'example03', 'example04', 'example05', 'example06',
+    'example07', 'example08', 'example10', 'example11', 'example12', 'example14', 'example16',
+    'example17', 'example20', 'example21', 'example22', 'example23']
 
   @property()
   onFlameNameChanged: ()=>void = ()=> {}
