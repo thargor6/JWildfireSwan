e9b9bf4d0c0c5aaffd6143deadbc09cd58b07adc
Andreas Maschke <thargor6@googlemail.com>
2022-02-24 21:17:54 +0100

    new variations, refactoring

--- frontend/flames/renderer/variations/variation-shaders-2d-complex.ts
+++ frontend/flames/renderer/variations/variation-shaders-2d-complex.ts
@@ -15,25 +15,20 @@
   02110-1301 USA, or see the FSF site: http://www.fsf.org.
 */
 
-import {VariationParam, VariationParamType, VariationShaderFunc2D, VariationTypes} from "./variation-shader-func";
-import {VariationShaders} from "Frontend/flames/renderer/variations/variation-shaders";
-import {RenderVariation, RenderXForm} from "Frontend/flames/model/render-flame";
 import {
-    FUNC_COSH,
-    FUNC_HYPOT,
-    FUNC_LOG10,
-    FUNC_MODULO,
-    FUNC_SGN,
-    FUNC_SINH,
-    FUNC_SQRT1PM1,
-    FUNC_TANH, LIB_COMPLEX
-} from "Frontend/flames/renderer/variations/variation-math-functions";
-import {M_PI} from "Frontend/flames/renderer/mathlib";
+    VariationParam,
+    VariationParamType,
+    VariationShaderFunc2D,
+    VariationShaderFunc3D,
+    VariationTypes
+} from './variation-shader-func';
+import {VariationShaders} from 'Frontend/flames/renderer/variations/variation-shaders';
+import {RenderVariation, RenderXForm} from 'Frontend/flames/model/render-flame';
+import {LIB_COMPLEX} from 'Frontend/flames/renderer/variations/variation-math-functions';
 
 /*
-  be sure to import this class somewhere and call register2DComplexVars()
+  be sure to import this class somewhere and call registerVars_Complex()
  */
-
 class AcosechFunc extends VariationShaderFunc2D {
     //acosech by Whittaker Courtney,
     //based on the hyperbolic variations by Tatyana Zabanova and DarkBeam's implementation of them.
@@ -56,7 +51,7 @@ class AcosechFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "acosech";
+        return 'acosech';
     }
 
     get funcDependencies(): string[] {
@@ -90,7 +85,7 @@ class AcoshFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "acosh";
+        return 'acosh';
     }
 
     get funcDependencies(): string[] {
@@ -119,7 +114,7 @@ class AcothFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "acoth";
+        return 'acoth';
     }
 
     get funcDependencies(): string[] {
@@ -163,7 +158,7 @@ class Arcsech2Func extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "arcsech2";
+        return 'arcsech2';
     }
 
     get funcDependencies(): string[] {
@@ -190,7 +185,7 @@ class ArcsinhFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "arcsinh";
+        return 'arcsinh';
     }
 
     get funcDependencies(): string[] {
@@ -225,7 +220,206 @@ class ArctanhFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "arctanh";
+        return 'arctanh';
+    }
+
+    get funcDependencies(): string[] {
+        return [LIB_COMPLEX];
+    }
+
+    get variationTypes(): VariationTypes[] {
+        return [VariationTypes.VARTYPE_2D];
+    }
+}
+
+class CombimirrorFunc extends VariationShaderFunc3D {
+    PARAM_VMIRROR = 'vmirror'
+    PARAM_VMOVE = 'vmove'
+    PARAM_HMIRROR = 'hmirror'
+    PARAM_HMOVE = 'hmove'
+    PARAM_ZMIRROR = 'zmirror'
+    PARAM_ZMOVE = 'zmove'
+    PARAM_PMIRROR = 'pmirror'
+    PARAM_PMOVEX = 'pmovex'
+    PARAM_PMOVEY = 'pmovey'
+    PARAM_VCOLORSHIFT = 'vcolorshift'
+    PARAM_HCOLORSHIFT = 'hcolorshift'
+    PARAM_ZCOLORSHIFT = 'zcolorshift'
+    PARAM_PCOLORSHIFT = 'pcolorshift'
+
+    get params(): VariationParam[] {
+        return [{ name: this.PARAM_VMIRROR, type: VariationParamType.VP_NUMBER, initialValue: 1.0 },
+            { name: this.PARAM_VMOVE, type: VariationParamType.VP_NUMBER, initialValue: 0.05 },
+            { name: this.PARAM_HMIRROR, type: VariationParamType.VP_NUMBER, initialValue: 0.50 },
+            { name: this.PARAM_HMOVE, type: VariationParamType.VP_NUMBER, initialValue: 0.35 },
+            { name: this.PARAM_ZMIRROR, type: VariationParamType.VP_NUMBER, initialValue: 0.0 },
+            { name: this.PARAM_ZMOVE, type: VariationParamType.VP_NUMBER, initialValue: 0.00 },
+            { name: this.PARAM_PMIRROR, type: VariationParamType.VP_NUMBER, initialValue: 0.00 },
+            { name: this.PARAM_PMOVEX, type: VariationParamType.VP_NUMBER, initialValue: 0.05 },
+            { name: this.PARAM_PMOVEY, type: VariationParamType.VP_NUMBER, initialValue: 0.00 },
+            { name: this.PARAM_VCOLORSHIFT, type: VariationParamType.VP_NUMBER, initialValue: 0.00 },
+            { name: this.PARAM_HCOLORSHIFT, type: VariationParamType.VP_NUMBER, initialValue: 0.0 },
+            { name: this.PARAM_ZCOLORSHIFT, type: VariationParamType.VP_NUMBER, initialValue: 0.00 },
+            { name: this.PARAM_PCOLORSHIFT, type: VariationParamType.VP_NUMBER, initialValue: 0.00 }]
+    }
+
+    getCode(xform: RenderXForm, variation: RenderVariation): string {
+        /*
+        Combination of vertical, horizontal, z mirror and point mirror
+        Mirror parameters work in range of 0..2
+        by Thomas Michels and the great support by Brad Stefanov
+        https://www.jwfsanctuary.club/custom-variations/custom-variation-combimirror-final-release/
+        */
+        return `{
+          float amount = float(${variation.amount});
+          float vmirror = float(${variation.params.get(this.PARAM_VMIRROR)});
+          float vmove = float(${variation.params.get(this.PARAM_VMOVE)});
+          float hmirror = float(${variation.params.get(this.PARAM_HMIRROR)});
+          float hmove = float(${variation.params.get(this.PARAM_HMOVE)});
+          float zmirror = float(${variation.params.get(this.PARAM_ZMIRROR)});
+          float zmove = float(${variation.params.get(this.PARAM_ZMOVE)});
+          float pmirror = float(${variation.params.get(this.PARAM_PMIRROR)});
+          float pmovex = float(${variation.params.get(this.PARAM_PMOVEX)});
+          float pmovey = float(${variation.params.get(this.PARAM_PMOVEY)});
+          float vcolorshift = float(${variation.params.get(this.PARAM_VCOLORSHIFT)});
+          float hcolorshift = float(${variation.params.get(this.PARAM_HCOLORSHIFT)});
+          float zcolorshift = float(${variation.params.get(this.PARAM_ZCOLORSHIFT)});
+          float pcolorshift = float(${variation.params.get(this.PARAM_PCOLORSHIFT)});
+          
+          Complex z;
+          Complex_Init(z, _tx, _ty);
+          Complex z2;
+          Complex_Init(z2, _tz, _ty);
+          Complex_Scale(z, amount);
+          Complex_Scale(z2, amount);
+          _vx = z.re;
+          _vy = z.im;
+          _vz = z2.re;
+          if(rand8(tex, rngState) > pmirror / 2.0) {
+            _vx = -_vx + pmovex;
+            _vy = -_vy + pmovey;
+            _color = mod(_color + pcolorshift, 1.0);
+          }
+          if(rand8(tex, rngState) < vmirror / 2.0) {
+            _vx = -_vx + vmove;
+            _color = mod(_color + vcolorshift, 1.0);
+          }
+          if(rand8(tex, rngState) < hmirror / 2.0) {
+            _vy = -_vy + hmove;
+            _color = mod(_color + hcolorshift, 1.0);
+          }
+          if(rand8(tex, rngState) < zmirror / 2.0) {
+            _vz = -_vz + zmove;
+            _color = mod(_color + zcolorshift, 1.0);
+          }
+        }`;
+    }
+
+    get name(): string {
+        return 'combimirror';
+    }
+
+    get funcDependencies(): string[] {
+        return [LIB_COMPLEX];
+    }
+
+    get variationTypes(): VariationTypes[] {
+        return [VariationTypes.VARTYPE_3D, VariationTypes.VARTYPE_DC];
+    }
+}
+
+class CSinFunc extends VariationShaderFunc2D {
+    PARAM_STRETCH = 'stretch'
+
+    get params(): VariationParam[] {
+        return [{ name: this.PARAM_STRETCH, type: VariationParamType.VP_NUMBER, initialValue: 1.5 }]
+    }
+
+    getCode(xform: RenderXForm, variation: RenderVariation): string {
+        // csin by 2010 Branden Brown, a.k.a. zephyrtronium
+        return `{
+              float amount = float(${variation.amount});
+              float stretch = float(${variation.params.get(this.PARAM_STRETCH)});
+              Complex c;     
+              Complex_Init(c,_tx * stretch ,_ty * stretch );
+              Complex_Sin(c);
+              _vx += amount * c.re;  
+              _vy += amount * c.im;
+        }`;
+    }
+
+    get name(): string {
+        return 'csin';
+    }
+
+    get funcDependencies(): string[] {
+        return [LIB_COMPLEX];
+    }
+
+    get variationTypes(): VariationTypes[] {
+        return [VariationTypes.VARTYPE_2D];
+    }
+}
+
+class JuliaOutsideFunc extends VariationShaderFunc2D {
+    PARAM_RE_DIV = 're_div'
+    PARAM_IM_DIV = 'im_div'
+    PARAM_MODE = 'mode'
+
+    get params(): VariationParam[] {
+        return [{ name: this.PARAM_RE_DIV, type: VariationParamType.VP_NUMBER, initialValue: 4.0 },
+            { name: this.PARAM_IM_DIV, type: VariationParamType.VP_NUMBER, initialValue: 0.0 },
+            { name: this.PARAM_MODE, type: VariationParamType.VP_NUMBER, initialValue: 0 }]
+    }
+
+    getCode(xform: RenderXForm, variation: RenderVariation): string {
+        //julia_outside by Whittaker Courtney
+        return `{
+          float amount = float(${variation.amount});
+          float re_div = float(${variation.params.get(this.PARAM_RE_DIV)});
+          float im_div = float(${variation.params.get(this.PARAM_IM_DIV)});
+          int mode = int(${variation.params.get(this.PARAM_MODE)});
+          Complex z, z2, z3;
+          Complex_Init(z ,_tx, _ty); 
+          Complex_Init(z2, _tx, _ty); 
+          Complex_Init(z3, re_div, im_div); 
+          if(mode == 0 || mode == 2) {
+            Complex_Sqrt(z);
+          }
+          Complex_Inc(z);
+          if(mode == 0 || mode == 2) {
+            Complex_Sqr(z);
+          } 
+          if(mode == 0 || mode == 2){ 
+            Complex_Sqrt(z2); 
+          }
+          Complex_Dec(z2);
+          if(mode == 0 || mode == 2) {
+            Complex_Sqr(z2);
+          }
+          Complex_Div(z,z2);
+          if(mode == 0 || mode == 1) {
+            Complex_Sqrt(z);
+          }
+          Complex_Div(z,z3);
+          if(mode == 0 || mode == 1) {
+            if(rand8(tex, rngState) < 0.5) {
+              _vx += amount * z.re; 
+              _vy += amount * z.im;
+            }
+            else{
+              _vx += amount * (-z.re);
+              _vy += amount * (-z.im);
+            }
+          } else {
+            _vx += amount * z.re; 
+            _vy += amount * z.im;   
+          }
+        }`;
+    }
+
+    get name(): string {
+        return 'julia_outside';
     }
 
     get funcDependencies(): string[] {
@@ -238,8 +432,8 @@ class ArctanhFunc extends VariationShaderFunc2D {
 }
 
 class PlusRecipFunc extends VariationShaderFunc2D {
-    PARAM_AR = "ar"
-    PARAM_AI = "ai"
+    PARAM_AR = 'ar'
+    PARAM_AI = 'ai'
 
     get params(): VariationParam[] {
         return [{ name: this.PARAM_AR, type: VariationParamType.VP_NUMBER, initialValue: 4.0 },
@@ -280,7 +474,7 @@ class PlusRecipFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "plusrecip";
+        return 'plusrecip';
     }
 
     get funcDependencies(): string[] {
@@ -314,7 +508,7 @@ class Sqrt_AcosechFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "sqrt_acosech";
+        return 'sqrt_acosech';
     }
 
     get funcDependencies(): string[] {
@@ -348,7 +542,7 @@ class Sqrt_AcoshFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "sqrt_acosh";
+        return 'sqrt_acosh';
     }
 
     get funcDependencies(): string[] {
@@ -382,7 +576,7 @@ class Sqrt_AcothFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "sqrt_acoth";
+        return 'sqrt_acoth';
     }
 
     get funcDependencies(): string[] {
@@ -416,7 +610,7 @@ class Sqrt_AsechFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "sqrt_asech";
+        return 'sqrt_asech';
     }
 
     get funcDependencies(): string[] {
@@ -450,7 +644,7 @@ class Sqrt_AsinhFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "sqrt_asinh";
+        return 'sqrt_asinh';
     }
 
     get funcDependencies(): string[] {
@@ -484,7 +678,7 @@ class Sqrt_AtanhFunc extends VariationShaderFunc2D {
     }
 
     get name(): string {
-        return "sqrt_atanh";
+        return 'sqrt_atanh';
     }
 
     get funcDependencies(): string[] {
@@ -496,13 +690,16 @@ class Sqrt_AtanhFunc extends VariationShaderFunc2D {
     }
 }
 
-export function register2DComplexVars() {
+export function registerVars_Complex() {
     VariationShaders.registerVar(new AcosechFunc())
     VariationShaders.registerVar(new AcoshFunc())
     VariationShaders.registerVar(new AcothFunc())
     VariationShaders.registerVar(new Arcsech2Func())
     VariationShaders.registerVar(new ArcsinhFunc())
     VariationShaders.registerVar(new ArctanhFunc())
+    VariationShaders.registerVar(new CombimirrorFunc())
+    VariationShaders.registerVar(new CSinFunc())
+    VariationShaders.registerVar(new JuliaOutsideFunc())
     VariationShaders.registerVar(new PlusRecipFunc())
     VariationShaders.registerVar(new Sqrt_AcosechFunc())
     VariationShaders.registerVar(new Sqrt_AcoshFunc())
