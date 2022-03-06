cb29cb15c544e96e7cd490ae1cfd69455547c23b
Andreas Maschke <thargor6@googlemail.com>
2022-03-06 13:06:50 +0100

    refactoring, removed unusede classes, switched from vite to webpack

--- src/main/java/org/jwildfire/swan/data/service/SampleAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package org.jwildfire.swan.data.service;
-
-import org.jwildfire.swan.data.entity.SampleAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SampleAddressRepository extends JpaRepository<SampleAddress, Integer> {
-
-}
--- src/main/java/org/jwildfire/swan/data/service/SamplePersonRepository.java
+++
@@ -1,8 +0,0 @@
-package org.jwildfire.swan.data.service;
-
-import org.jwildfire.swan.data.entity.SamplePerson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SamplePersonRepository extends JpaRepository<SamplePerson, Integer> {
-
-}
