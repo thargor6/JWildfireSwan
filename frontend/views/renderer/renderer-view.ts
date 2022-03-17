84fd24ed6eb86677816220d35be33e68bdba566f
Andreas Maschke <thargor6@googlemail.com>
2022-03-17 20:58:24 +0100

    added support for cropping the final rendered images in the batch renderer

--- frontend/views/renderer/renderer-view.ts
+++ frontend/views/renderer/renderer-view.ts
@@ -29,27 +29,36 @@ import '@vaadin/vaadin-progress-bar'
 import {RendererFlame, rendererStore} from "Frontend/stores/renderer-store";
 import '../../components/render-panel'
 import '../../components/swan-error-panel'
-import {CropRegion, FlameRenderer} from "Frontend/flames/renderer/flame-renderer";
+import {FlameRenderer} from "Frontend/flames/renderer/flame-renderer";
 import {DisplayMode} from "Frontend/flames/renderer/render-settings";
 import {RenderPanel} from "Frontend/components/render-panel";
 import {autorun} from "mobx";
 import {state} from "lit/decorators";
 import {HasValue} from "@hilla/form";
 import '../../components/swan-loading-indicator';
+import {RenderResolution, RenderResolutions} from "Frontend/flames/renderer/render-resolution";
 
 @customElement('renderer-view')
 export class RendererView extends View  {
     @state()
-    imageSize = 512
+    renderSize = RenderResolutions.defaultRenderSize
 
-    imageSizes = [128, 256, 512, 1024, 2048]
+    renderSizes = RenderResolutions.renderSizes
 
     @state()
-    swarmSize = 256
+    swarmSize = RenderResolutions.defaultSwarmSize
 
-    swarmSizes = [8, 16, 32, 64, 128, 256, 512, 1024]
+    swarmSizes = RenderResolutions.swarmSizes
 
-    private cropRegion: CropRegion | undefined
+    @state()
+    cropSize = RenderResolutions.defaultRenderResolution
+
+    cropSizes = RenderResolutions.getRenderResolutions(this.renderSize)
+
+    @state()
+    qualityScale = 1.5
+
+    qualityScaleValues = [0.5, 1.0, 1.5, 2.0, 4.0, 8.0, 16.0]
 
     @state()
     autoSave = true
@@ -69,15 +78,24 @@ export class RendererView extends View  {
                       <renderer-upload-panel style="border-radius: var(--lumo-border-radius); border: 1px dashed gray;"></renderer-upload-panel>
                       <div style="margin-top: 0.5em; display: flex; flex-direction: column; padding: 1.0em; border-radius: var(--lumo-border-radius); border: 1px dashed gray;">
                          <div style="display: flex;"> 
-                             <vaadin-combo-box style="max-width: 10em;" label="Image size" .items="${this.imageSizes}" value="${this.imageSize}"
-                                @change="${(event: Event) => this.imageSizeChanged(event)}"></vaadin-combo-box>
-                             <vaadin-combo-box style="max-width: 10em; margin-left: 1em;" label="Swarm size" .items="${this.swarmSizes}" value="${this.swarmSize}"
-                               @change="${(event: Event) => this.pointsSizeChanged(event)}"></vaadin-combo-box>
+                             <vaadin-combo-box style="max-width: 10em;" label="Render size" .items="${this.renderSizes}" value="${this.renderSize}"
+                                               @change="${(event: Event) => this.renderSizeChanged(event)}"></vaadin-combo-box>
+                             <vaadin-combo-box style="max-width: 10em; margin-left: 1em;" label="Crop size"
+                                               .items="${this.cropSizes!.map(cropSize => cropSize.displayName)}" value="${this.cropSize.displayName}"
+                                               @change="${(event: Event) => this.cropSizeChanged(event)}"></vaadin-combo-box>
                           </div>
-                          <vaadin-checkbox ?checked=${this.autoSave} label="Automatic download generated files" @change="${(event: Event) => this.autoSaveChanged(event)}"></vaadin-checkbox>
                           <div style="display: flex;">
+                            <vaadin-combo-box style="max-width: 10em;" label="Swarm size" .items="${this.swarmSizes}" value="${this.swarmSize}"
+                                            @change="${(event: Event) => this.pointsSizeChanged(event)}"></vaadin-combo-box>
+                              <vaadin-combo-box style="max-width: 10em; margin-left: 1em;" label="Quality scale"
+                                                .items="${this.qualityScaleValues}" value="${this.qualityScale}"
+                                                @change="${(event: Event) => this.qualityScaleChanged(event)}"></vaadin-combo-box>
+
+                          </div>
+                          <vaadin-checkbox style="margin-top: 1em;" ?checked=${this.autoSave} label="Automatic download generated files" @change="${(event: Event) => this.autoSaveChanged(event)}"></vaadin-checkbox>
+                           <div style="display: flex; margin-top: 1em; margin-bottom: 1em;">
                              <vaadin-button @click="${this.renderFlames}">Render</vaadin-button>
-                           <vaadin-button theme="tertiary" @click="${this.signalCancel}">Cancel</vaadin-button>
+                             <vaadin-button theme="tertiary" @click="${this.signalCancel}">Cancel</vaadin-button>
                           </div>    
                          <vaadin-progress-bar value="${rendererStore.renderProgress}"></vaadin-progress-bar>
                          <swan-loading-indicator .loading=${rendererStore.rendering} caption="${rendererStore.cancelSignalled ? 'Aborting...' : 'Rendering...'}"></swan-loading-indicator>
@@ -87,7 +105,6 @@ export class RendererView extends View  {
                        <renderer-render-panel></renderer-render-panel>
                        <render-panel style="margin: 1em;" .withProgressBar="${false}" containerWidth="12.5em" containerHeight="12.5em"
                                      canvasDisplayWidth="12em" canvasDisplayHeight="12em" .onCreateFlameRenderer=${this.createFlameRenderer}></render-panel>
-
                    </div>
                   <div class="gap-m grid list-none m-2 p-2" style="grid-template-columns: repeat(auto-fill, minmax(7em, 1fr));" id="allImageContainer"></div>
 
@@ -96,9 +113,23 @@ export class RendererView extends View  {
         `;
     }
 
-    private imageSizeChanged(event: Event) {
+    private renderSizeChanged(event: Event) {
         if ((event.target as HasValue<string>).value) {
-            this.imageSize = parseInt((event.target as HasValue<string>).value!)
+            this.renderSize = parseInt((event.target as HasValue<string>).value!)
+            this.cropSizes = RenderResolutions.getRenderResolutions(this.renderSize)
+            this.cropSize = this.cropSizes![0]
+        }
+    }
+
+    private qualityScaleChanged(event: Event) {
+        if ((event.target as HasValue<string>).value) {
+            this.qualityScale = parseFloat((event.target as HasValue<string>).value!)
+        }
+    }
+
+    private cropSizeChanged(event: Event) {
+        if ((event.target as HasValue<string>).value) {
+            this.cropSize = RenderResolutions.findRenderResolutionByName(this.renderSize, (event.target as HasValue<string>).value!)
         }
     }
 
@@ -128,7 +159,7 @@ export class RendererView extends View  {
     createFlameRenderer = ()=> {
         return new FlameRenderer(256, 256,
           DisplayMode.FLAME, this.getRenderPanel().canvas, undefined,
-          false, undefined, rendererStore.selectedFlames[0].flame)
+          false, undefined, 1.0, rendererStore.selectedFlames[0].flame)
     }
 
     getRenderPanel = (): RenderPanel =>  {
@@ -152,11 +183,11 @@ export class RendererView extends View  {
             const renderPanel = this.getRenderPanel()
             renderPanel.clearRenderer()
             this.getRenderPanel().recreateCanvas()
-            this.renderer =  new FlameRenderer(this.imageSize, this.swarmSize,
+            this.renderer =  new FlameRenderer(this.renderSize, this.swarmSize,
               DisplayMode.FLAME, this.getRenderPanel().canvas, this.imageContainer,
-              true, this.cropRegion, flame.flame)
+              true, RenderResolutions.getCropRegion(this.renderSize, this.cropSize),
+              this.qualityScale, flame.flame)
             this.renderer.onRenderFinished=this.onFlameFinished.bind(this, flame)
-            this.renderer.qualityScale = 2.0
             this.getRenderPanel().rerenderFlame(this.renderer)
         }
         else {
