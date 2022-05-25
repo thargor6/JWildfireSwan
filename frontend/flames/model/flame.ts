6220ca9bd8d3f7450b7ca2ecfc7443bb3615bef0
Andreas Maschke <thargor6@googlemail.com>
2022-05-26 01:28:30 +0200

    -disabled the SmartDenoiser-slider per default -ready to deploy new version on Heroku

--- frontend/flames/model/flame.ts
+++ frontend/flames/model/flame.ts
@@ -188,7 +188,7 @@ export class Flame {
     public fps = Parameters.intParam(25);
 
     public dnType = DenoiserType.SMART_DENOISE_SRGB;
-    public dnSplitter = Parameters.floatParam(0.0);
+    public dnSplitter = Parameters.floatParam(-1.0);
     public dnSigma = Parameters.floatParam(2.1);
     public dnKSigma  = Parameters.floatParam(1.15);
     public dnThreshold = Parameters.floatParam(0.165);
