d617bfa712e6c361d8e66ceaae07ea35790668a7
Andreas Maschke <thargor6@googlemail.com>
2022-06-16 23:47:43 +0200

    -upgraded to hilla 1.1 -removed PWA-annotation from the main application

--- src/main/java/org/jwildfire/swan/Application.java
+++ src/main/java/org/jwildfire/swan/Application.java
@@ -19,7 +19,6 @@ package org.jwildfire.swan;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -33,10 +32,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 @SpringBootApplication(scanBasePackages = "org.jwildfire")
 @Theme(value = "jwildfireswan")
-@PWA(
-    name = "JWildfire Swan",
-    shortName = "JWildfire Swan",
-    offlineResources = {"images/logo.png"})
 @NpmPackage(value = "@polymer/paper-slider", version = "3.0.1")
 @JsModule("@polymer/paper-slider/paper-slider.js")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
