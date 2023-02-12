3a09c73514022eb61a3108cc4fc80e3b4006858e
thargor6 <thargor6@googlemail.com>
2023-02-12 11:33:02 +0100

    creating a prototype using hilla-react: basic setup of the project

--- src/main/java/org/jwildfire/swan/Application.java
+++ src/main/java/org/jwildfire/swan/Application.java
@@ -1,42 +1,43 @@
-/*
-  JWildfire Swan - fractal flames the playful way, GPU accelerated
-  Copyright (C) 2021-2022 Andreas Maschke
-
-  This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser
-  General Public License as published by the Free Software Foundation; either version 2.1 of the
-  License, or (at your option) any later version.
-
-  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public License along with this software;
-  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  02110-1301 USA, or see the FSF site: http://www.fsf.org.
-*/
 package org.jwildfire.swan;
 
-import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.dependency.NpmPackage;
+import org.jwildfire.swan.data.service.UserRepository;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
+import org.springframework.context.annotation.Bean;
 
 /**
  * The entry point of the Spring Boot application.
  *
- * <p>Use the @PWA annotation make the application installable on phones, tablets and some desktop
- * browsers.
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
  */
-@SpringBootApplication(scanBasePackages = "org.jwildfire")
-@Theme(value = "jwildfireswan")
-@NpmPackage(value = "@polymer/paper-slider", version = "3.0.1")
-@JsModule("@polymer/paper-slider/paper-slider.js")
-public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+@SpringBootApplication
+@Theme(value = "jwildfire-swan")
+public class Application implements AppShellConfigurator {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
+            SqlInitializationProperties properties, UserRepository repository) {
+        // This bean ensures the database is only initialized when empty
+        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties) {
+            @Override
+            public boolean initializeDatabase() {
+                if (repository.count() == 0L) {
+                    return super.initializeDatabase();
+                }
+                return false;
+            }
+        };
+    }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { UserConfigFn } from 'vite';
+import react from '@vitejs/plugin-react';
+import type { UserConfigFn } from 'vite';
 import { overrideVaadinConfig } from './vite.generated';
 
 const customConfig: UserConfigFn = (env) => ({
   // Here you can add custom Vite parameters
   // https://vitejs.dev/config/
+  plugins: [
+    react({
+      include: '**/*.tsx',
+    }),
+  ],
 });
 
 export default overrideVaadinConfig(customConfig);
