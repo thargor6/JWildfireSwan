9df98b4a7aa18495a989b08bccebb1b012d0c398
thargor6 <thargor6@googlemail.com>
2023-02-12 18:02:32 +0100

    creating a prototype using hilla-react: final project setup, ready to start with the real react-development now!

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,28 +1,5 @@
-import { Router } from '@vaadin/router';
-import { routes } from './routes';
-import { appStore } from './stores/app-store';
-import {configureLocalization} from '@lit/localize';
+import { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.js';
 
-export const router = new Router(document.querySelector('#outlet'));
-
-router.setRoutes(routes);
-
-window.addEventListener('vaadin-router-location-changed', (e) => {
-  appStore.setLocation((e as CustomEvent).detail.location);
-  const title = appStore.currentViewTitle;
-  if (title) {
-    document.title = title + ' | ' + appStore.applicationName;
-  } else {
-    document.title = appStore.applicationName;
-  }
-});
-
-export const {getLocale, setLocale} = configureLocalization({
-  sourceLocale: 'en',
-  targetLocales: ['de'],
-  loadLocale: (locale: string) => import(`../frontend/locales/${locale}.ts`),
-});
-
-(async () => {
-  //await setLocale('de');
-})();
+createRoot(document.getElementById('outlet')!).render(createElement(App));
--- frontend/views/about/AboutView.tsx
+++ frontend/views/about/AboutView.tsx
@@ -1,7 +1,7 @@
 export default function AboutView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
+      <img style={{ width: '200px' }} src="icons/swan_logo_200.png" />
       <h2>This place intentionally left empty</h2>
       <p>It’s a place where you can grow your own UI 🤗</p>
     </div>
